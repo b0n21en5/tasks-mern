d45d48fe025eae7be1c6645e27844421abed1dbd
Bikash <splittergame91@gmail.com>
2023-12-26 23:53:03 +0530

    fix: fixed UI and implemented responsive design

--- frontend/src/components/Task/Task.jsx
+++ frontend/src/components/Task/Task.jsx
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {
   MdOutlineCheckBox,
   MdOutlineCheckBoxOutlineBlank,
 } from "react-icons/md";
+import { updateTaskRoute } from "../../utills/apiRoutes";
+import axios from "axios";
 import styles from "./Task.module.css";
 
 const Task = ({ task }) => {
+  const [isDone, setIsDone] = useState(null);
+
+  useEffect(() => {
+    setIsDone(task.isDone);
+  }, []);
+
+  const updateTask = async () => {
+    try {
+      const { data } = await axios.put(`${updateTaskRoute}/${task._id}`, {
+        isDone: isDone,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (isDone !== null) {
+      updateTask();
+    }
+  }, [isDone, task._id]);
+
   return (
     <Link className={styles.task} to={`/task-details/${task._id}`}>
       {task.name}
-      {task.isDone ? <MdOutlineCheckBox /> : <MdOutlineCheckBoxOutlineBlank />}
+      {isDone ? (
+        <MdOutlineCheckBox
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            setIsDone(false);
+          }}
+        />
+      ) : (
+        <MdOutlineCheckBoxOutlineBlank
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            setIsDone(true);
+          }}
+        />
+      )}
     </Link>
   );
 };
